9b6e732ca88cb76cb548b99927ca7fac1214383c
dahmana <dhabooode@gmail.com>
2024-05-09 13:56:58 -0700

    first

---
+++ src/components/SelectedContact.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useState } from "react"
+import { useEffect } from "react"
+
+
+function SelectedContact(){({ selectedContactId, setSelectedContactId }) => {
+    return (
+      <div>
+        {/* Your component JSX here */}
+      </div>
+    );
+  };
+
+}
+
+
+
+
+
+export default SelectedContact
